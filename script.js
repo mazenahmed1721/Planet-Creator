76e2927df60fa3d7fa9dd24f132bc7f7c815963f
Mazen Khalil <mazenahmed1721@gmail.com>
2026-09-16 11:25:51 +0300

    Add planet size control

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let planet = {
   type: "earth",
+  size: 180
 };
 const planetEl = document.getElementById("planet");
 const typeButtons = document.querySelectorAll(".type-btn");
+const sizeSlider = document.getElementById("sizeSlider");
 typeButtons.forEach(function (btn) {
   btn.addEventListener("click", function () {
     typeButtons.forEach(function (b) {
@@ -13,6 +15,12 @@ typeButtons.forEach(function (btn) {
     updatePlanet();
   });
 });
+sizeSlider.addEventListener("input", function () {
+  planet.size = parseInt(sizeSlider.value);
+  updatePlanet();
+});
 function updatePlanet() {
   planetEl.className = "planet " + planet.type;
+  planetEl.style.width = planet.size + "px";
+  planetEl.style.height = planet.size + "px";
 }
